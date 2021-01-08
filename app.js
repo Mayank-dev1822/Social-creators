6625532861a802000065ddd5e6b1235ec83f67a6
Mayank-dev1822 <mayanksainimayank56@gmail.com>
2021-01-08 11:20:11 +0530

    changed port to env process

--- app.js
+++ app.js
@@ -436,6 +436,4 @@ app.post("/login", function (req, res) {
 //     console.log("Server started at port 3000");
 // });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT);
