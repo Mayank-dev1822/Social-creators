3a72f7fca39f7adca6a2ec5a1df17823fb98adfe
Mayank-dev1822 <mayanksainimayank56@gmail.com>
2021-01-09 01:35:30 +0530

    removed quoma

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/devbase",
+    callbackURL: "http://localhost:3000/auth/google/devbase"
     // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
